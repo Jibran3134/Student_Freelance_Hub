7d615e356e2ff99dc2fd0336323bb77995ccbc14
Muhammad Jibran <muhammad.jibran.sqb@gmail.com>
2025-10-31 19:03:49 +0500

    Delete Backend directory

--- Backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-// main logic connecting ths backend logic to services and DB
- //when we go on a certain url, and a specific service is called from the controller, perform the following things
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'This is my first NestJS app';
-  }
-}
